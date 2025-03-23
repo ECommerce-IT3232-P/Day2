538aa7f9bb29673243f87bcbf53afa733ad01581
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2025-03-23 14:57:34 +0530

    Added AppController with /msg and /name endpoints

---
+++ myApp/src/main/java/test/java/lk/ac/vau/fas/it/controller/AppController.java
@@ -0,0 +1,18 @@
+package test.java.lk.ac.vau.fas.it.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+@GetMapping("/msg")
+public String myMessage() {
+    return "Hello Springboot";
+}
+@GetMapping("/name")
+public String myName() {
+    return "My Name is Springboot";
+}
+}
